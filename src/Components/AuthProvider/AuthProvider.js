4be80f4ac847e744a686d5e390a0e4f82d5ab9c6
Musabbir-pr <musabbir313@gmail.com>
2023-03-12 15:08:54 +0600

    added authenticaiton methods

--- src/Components/AuthProvider/AuthProvider.js
+++ src/Components/AuthProvider/AuthProvider.js
@@ -1,18 +1,72 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import app from "../../firebase/firebase.config";
-import { getAtuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GithubAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
-const auth = getAtuth(app);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useContext(null);
+  const [loading, setLoading] = useState(true);
 
-const authInfo= {
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-}
+  //Login user with email password
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
+  //Login with gmail
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
-  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+  //signOut
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //manage user
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user, 
+    loading,
+    createUser,
+    login,
+    logOut,
+    googleLogin
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
