34092699cadb268fd40d09a65ed33e4debfa8cb7
Musabbir-pr <musabbir313@gmail.com>
2023-03-12 14:24:09 +0600

    initialized project and added firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWfkDcxH6jdmnxQRERVA-YAZSRMZYB7PM",
+  authDomain: "findmentor-236d0.firebaseapp.com",
+  projectId: "findmentor-236d0",
+  storageBucket: "findmentor-236d0.appspot.com",
+  messagingSenderId: "284277222426",
+  appId: "1:284277222426:web:0b0250ab252746bb4ef5a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app 
